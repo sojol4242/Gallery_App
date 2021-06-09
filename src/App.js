c6d4c798560a4ae06ca83f38264df719bd1c9743
Md Moniruzzaman Sojol <bsmrstueee90@gmail.com>
2021-06-09 15:27:20 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=17475640-f7b4730b8a058e6d7688ea10f&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container m-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ?
+      <img src="https://i.ibb.co/wYTXbJs/Ripple-1-7s-307px.gif" alt="loading"   className="text-center mx-auto mt-32" />: <div className="flex flex-wrap">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
+    </div>
+  );
+}
+
+export default App;
